f7fbe663de6fc2b252a4fc09760460f456543649
!TYL3Rk <115991724+1TYL3Rk@users.noreply.github.com>
2025-11-29 22:52:11 +0700

    redesign

---
+++ script.js
@@ -0,0 +1,25 @@
+new Vue({
+    el: '#app',
+    data: {
+        profileData: {
+            name: '!TYL3Rk',
+            description: 'I have been a FiveM game developer since 2021.'
+        },
+        socialLinks: [
+            { name: 'GitHub', icon: 'fab fa-github', url: 'https://github.com/1TYL3Rk' },
+            { name: 'Instagram', icon: 'fab fa-instagram', url: 'https://www.instagram.com/1tyl3rk_' },
+            { name: 'Discord: _azaretz', icon: 'fab fa-discord', url: 'https://www.discord.com' }
+        ],
+        skills: [
+            // { name: 'Vue.js', level: 90 },
+            // { name: 'JavaScript', level: 85 },
+            // { name: 'HTML/CSS', level: 95 },
+            // { name: 'Node.js', level: 80 },
+            // { name: 'Python', level: 75 },
+            // { name: 'Git', level: 85 }
+            { name: 'Lua [FiveM]', level: 80 },
+            { name: 'HTML/CSS', level: 60 },
+            { name: 'Javascript', level: 30 }
+        ]
+    }
+});
